e19b3261137625d6dac40d71f4433848641ebc65
naofumi0115 <65013080+naofumi0115@users.noreply.github.com>
2020-05-22 13:40:42 +0900

    Update SOLID_No2.php

--- SOLID_No2.php
+++ SOLID_No2.php
@@ -1,49 +1,53 @@
 <!DOCTYPE html>
 <html>
+
 <body>
   <?php
-  interface WorkAbleInterface{
-      public  function work();
+  interface WorkAbleInterface
+  {
+    public  function work();
   }
 
-  interface SleepAbleInterface{
-      public  function  sleep();
+  interface SleepAbleInterface
+  {
+    public  function  sleep();
   }
 
-  interface Electricity{
+  interface Electricity
+  {
     public function hasPower();
   }
 
-  class Workers implements WorkAbleInterface{
 
+
+  class HumanWorker  implements SleepAbleInterface , WorkAbleInterface{
+    
     public  function work(){
-    var_dump('works');
-  }
-  }
-
-  class HumanWorker extends Workers implements SleepAbleInterface{
-
-      public  function  sleep(){
-          var_dump('sleep');
-      }
-
-  }
-
-  class RobotWorker extends Workers implements Electricity{
-      public  function work(){
-          if ($this->hasPower()) {
-            parent::work();
-          }
-      }
-
-      public  function hasPower()
-      {
-
-          // return true if robot have power, otherwise false.
+      var_dump("works");
     }
 
+    public function  sleep()
+    {
+      var_dump('sleep');
+    }
   }
 
-?>
+  class RobotWorker  implements Electricity , WorkAbleInterface{
+
+    public  function work(){
+      if ($this->hasPower()) {
+        var_dump("works");
+      }
+    }
+
+    public  function hasPower()
+    {
+
+      // return true if robot have power, otherwise false.
+    }
+  }
+
+  ?>
 </body>
+
 </html>
