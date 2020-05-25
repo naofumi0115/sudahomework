a9d34ef9cdacbf37bcfefd63f0abdec0dafba109
naofumi0115 <65013080+naofumi0115@users.noreply.github.com>
2020-05-25 10:53:53 +0900

    Update OOP_No1.php

--- OOP_No1.php
+++ OOP_No1.php
@@ -1,51 +1,45 @@
 <!DOCTYPE html>
 <html>
+
 <body>
   <?php
 
-  interface calc {
-
-    public function area();
-
-  }
-
-  class calc_square implements calc {
+  class shape{
 
     public $vertical;
     public $width;
 
-    public function __construct($vertical,$width){
+    public function __construct($vertical, $width){
       $this->vertical = $vertical;
       $this->width = $width;
-    }
-
-    public function area(){
     }
 
   }
 
 
-  class calc_triangle extends calc_square{
-    public function area(){
+  class triangle extends shape{
+    public function area()
+    {
       $triangle = $this->vertical * $this->width / 2;
       echo $triangle;
       echo "<br>";
     }
   }
 
-  class calc_rectangle extends calc_square{
-    public function area(){
+  class rectangle extends shape{
+    public function area()
+    {
       $rectangle = $this->vertical * $this->width;
       echo $rectangle;
       echo "<br>";
     }
   }
 
-  $triangle = new calc_triangle(10,10);
+  $triangle = new triangle(10, 10);
   $triangle->area();
 
-  $rectangle = new calc_rectangle(10,10);
+  $rectangle = new rectangle(10, 10);
   $rectangle->area();
-?>
+  ?>
 </body>
 </html>
