c8f17030a3d60c0fc233886b6bc535f2ef8b9af3
naofumi0115 <65013080+naofumi0115@users.noreply.github.com>
2020-05-25 10:39:54 +0900

    Update array_No2.php

--- array_No2.php
+++ array_No2.php
@@ -3,29 +3,22 @@
 <body>
   <?php
 
-
-  function arr_calc($arr_1 , $arr_2){
+  function arr_calc($arr_1, $arr_2)
+  {
 
     $arr_sum = array();
 
-
-    for ($index = 0 ; $index <  count($arr_1) ; $index++){
+    for ($index = 0; $index <  count($arr_1); $index++) {
       $sum = $arr_1[$index] + $arr_2[$index];
-      array_push($arr_sum, $sum);
+      echo $sum . " ";
     }
 
-    for ($index = 0 ; $index <  count($arr_1) ; $index++){
-      echo $arr_sum[$index] . "  ";
-    }
   }
 
-  $arr_1 = array(1,2,3,4,5);
-  $arr_2 = array(4,5,3,2,15);
-  arr_calc($arr_1 , $arr_2);
+  $arr_1 = array(1, 2, 3, 4, 5);
+  $arr_2 = array(4, 5, 3, 2, 10);
+  arr_calc($arr_1, $arr_2);
 
-
-
-
-?>
+  ?>
 </body>
 </html>
