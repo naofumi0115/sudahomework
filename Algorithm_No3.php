d36bf734a183d0c6c43d0ba7711d5e62a3ff1173
naofumi0115 <65013080+naofumi0115@users.noreply.github.com>
2020-06-01 10:32:40 +0900

    Update Algorithm_No3.php

--- Algorithm_No3.php
+++ Algorithm_No3.php
@@ -1,29 +1,53 @@
 <!DOCTYPE html>
 <html>
+
 <body>
   <?php
 
-  function Pair($arr,$num){
-
-    $bool = FALSE;
-
-    for ($i = 0;$i < count($arr);$i++){
-      for($n = $i;$n < count($arr);$n++){
-        if($num == $arr[$i] + $arr[$n]){
-          echo "There is a pair " . $arr[$i] . " " . $arr[$n] . "with sum" . $num;
-          echo "<br>";
-          $bool = TRUE;
+  function Pair($arr , $num)
+  {
+    $max = 0;
+    $min = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+      if ($arr[$i + 1] < $arr[$i]) {
+        $max = $i;
+        $min = $i + 1;
         break;
-        }
-      }
-      if ($bool){
-      break;
       }
     }
+
+    for($i = 0 ; $max != $min ; $i++){
+      $sum = $arr[$max] + $arr[$min];
+       if($sum == $num){
+        echo "There is a pair " . $arr[$max] . " " . $arr[$min] . "with sum" . $num;
+        echo "<br>";
+       break;
+       }else{
+         if($sum < $num){
+           if($min  != count($arr) - 1 ){
+            $min = $min + 1;
+           }else{
+             $min = 0;
+           }
+         }else{
+           if($max != 0){
+            $max = $max - 1;
+           }else{
+            $max = count($arr) - 1;
+           }
+         }
+       }
+    }
+
+
   }
 
-  $arr = array(11,15,6,8,9,10);
-  Pair($arr , 16);
+  $arr = array(11, 15, 6, 8, 9, 10);
+  Pair($arr,21);
+
+  $arr = array(11, 15, 26, 38, 9, 10);
+  Pair($arr,35);
   ?>
 </body>
+
 </html>
