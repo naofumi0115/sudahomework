7c72161eccbb3d0731f1390d1d7bfd879f9f9f31
naofumi0115 <naofumi.suda@pascalia.co.jp>
2020-06-29 09:30:18 +0900

    Update index.php

--- pav-training/index.php
+++ pav-training/index.php
@@ -6,7 +6,7 @@ session_start();
 
 $parsed_url = parse_url($_SERVER['REQUEST_URI']);
 
-if (!isset($_SESSION['LOGGED_IN']) && $parsed_url['path'] != '/' && $parsed_url['path'] != '/login') {
+if (!isset($_SESSION['LOGGED_IN']) && $parsed_url['path'] != '/' && $parsed_url['path'] != '/login' && $parsed_url['path'] != '/logout') {
     header("Location: /");
     die();
 }
@@ -26,20 +26,16 @@ Route::add('/users', function () {
 Route::add('/login', function () {
     if (!empty($_POST)) {
 
-        if("logout" == $_POST['log']){
-            $_SESSION['LOGGED_IN'] = null;
-            $_SESSION['USERNAME'] = null;
-        }
-
         $username = $_POST['username'];
         $pass = $_POST['password'];
 
         if (isset($username) && isset($pass)) {
             if (isUserValid($username, $pass)) {
                 $_SESSION['LOGGED_IN'] = $username;
-                $_SESSION['USERNAME'] = $_POST['username'];
+                $_SESSION['USERNAME'] = $username;
                 }
             if (isset($_POST['rememberUsername'])) {
+                $username = mb_strtolower($username);
                 $_SESSION['rem_name'] = $username;
                 setcookie('username', $username, time() + (24 * 60 * 60 * 5), "/"); // 5 days
                 }else{
@@ -47,19 +43,23 @@ Route::add('/login', function () {
                 }
             }
         }
-                header('Location: /');
-                exit;
-            }, 'post');
+        header('Location: /');
+        exit;
+    }, 'post');
+
+Route::add('/logout', function () {
+    if (!empty($_POST)) {
+        $_SESSION['LOGGED_IN'] = null;
+        $_SESSION['USERNAME'] = null;
+    }
+    header('Location: /');
+    exit;
+}, 'post');
 
             function isUserValid($username, $password)
             {
                 return $username == 'pav' && $password == '123';
             }
-
-Route::add('/logout', function () {
-    $_SESSION['LOGGED_IN'] = null;
-    $_SESSION['USERNAME'] = null;
-},'post');
 
             Route::run('/');
 ?>
